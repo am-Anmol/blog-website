070ba6cc2a8eea8fa00c5e038222f952cdf1ec7a
ananya-dak <ananyadakamuri@gmail.com>
2022-11-22 20:08:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #import libraries
 from flask import *
 from flask_mysqldb import MySQL
+import time 
 import MySQLdb.cursors
 app=Flask(__name__)
-app.secret_key = "4db8b51a4017e427f3ea5c2137c450f767dce1bf"  
 
 #code for connection
 app.config['MYSQL_HOST'] = 'localhost'#hostname
@@ -22,10 +22,6 @@ def index():
 @app.route('/register')
 def register():
     return render_template('register.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
 @app.route('/savedetails',methods=['GET', 'POST'])
 
@@ -49,25 +45,24 @@ def savedetails():
 def add():
     return render_template('addblog.html')
 
-@app.route('/logged',methods=['GET', 'POST'])
 
-def loggedin():
+@app.route('/saveblog',methods=['GET', 'POST'])
+def saveblog():
     if request.method == "POST":
-            EMAIL = request.form['email']
-            PSS = request.form['password']
+            BT = request.form['blogtitle']
+            BC = request.form['blogcontent']
+            IMG = request.form['imagefile']
             cur = mysql.connection.cursor()
-
-            cur.execute('SELECT * FROM user WHERE email = %s AND password = %s', (EMAIL, PSS,))
-            account = cur.fetchone()
-            if account:
-                session['loggedin'] = True
-                session['userid']=account[0]
-                session['email'] = account[2]
-                return 'Logged in successfully!'+session['email']
             
-            else:
-                return 'Incorrect username/password!'
-    
+            
+            blogTitle = BT
+            blogDesc = BC
+            blogImg = IMG
+            createTime = time.strftime('%Y-%m-%d %H:%M:%S')
+            cur.execute("INSERT INTO User(Name, Email, Password) VALUES (%s, %s, %s)", (Name, Email, Password))
+            mysql.connection.commit()
+            cur.close()
+            return 'success'
     
 
 app.run(debug=True)
