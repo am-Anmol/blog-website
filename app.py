e53d6de9fdfdac0002da6455467a04517c92f176
Anmol Gupta <guptaanmol248001@gmail.com>
2022-11-22 19:12:32 +0530

    added database file

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import re
 from flask import *
+import database
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 app.run(debug=True)
