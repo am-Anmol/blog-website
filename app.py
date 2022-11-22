8392c4bfcca4f073100af9b64407546d4af1ed03
ananya-dak <ananyadakamuri@gmail.com>
2022-11-22 18:23:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 app.run(debug=True)
